6fd7e709661190bd051818baa2664aed9d6c5d1b
Ayush <ayush.raj.sd@gmail.com>
2024-11-04 22:19:36 +0530

    get and post

--- class2/index.js
+++ class2/index.js
@@ -2,11 +2,25 @@ const express = require("express");
 
 // create an express app
 const app = express();
+// purpose of express.json() is to parse the incoming JSOB reqest and make the data available under req.body
+app.use(express.json());
 
 // define a route
 app.get("/", (req, res) => {
   res.send("hello, Express!");
 });
+
+app.get("/about", (req, res) => {
+  res.send("this is about page");
+});
+
+app.post("/data", (req, res) => {
+  //   console.log(req);
+  console.log(req.body);
+  res.send("this is post request");
+});
+
+// RESTful API - use proper verbs for the routes / request
 
 const port = 3000;
 app.listen(port, () => {
